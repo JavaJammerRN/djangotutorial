27d0751ea55d793e5c5b1bb1d6f6ad8e476e7efa
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-09-22 14:46:54 +0100

    Added create which saves bookings

--- booking/forms.py
+++ booking/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.forms import widgets
 from .models import Booking
+from django.contrib.admin import widgets
 
 
 class SearchBookingForm(forms.Form):
-	user_name = forms.CharField(label='Username', widget=forms.TextInput(attrs={'placeholder': 'username'}), max_length=140)
+    user_name = forms.CharField(label='Username', widget=forms.TextInput(attrs={'placeholder': 'username'}), max_length=140)
 
 class CreateBookingForm(forms.ModelForm):
-	class Meta:
-		model = Booking
-		fields = '__all__'
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        widgets = {
+            'date': forms.DateInput(attrs={'class':'date'}),
+        }
 
--- booking/views.py
+++ booking/views.py
@@ -8,6 +8,17 @@ def booking_create(request):
     booking_form = CreateBookingForm()
     context = {'form' : booking_form}
 
+    if request.method == 'POST':
+        form = CreateBookingForm(request.POST)
+
+        if form.is_valid():
+            booking = form.save()
+            messages.success(request, 'Booking has been created!')
+
+        else:
+            messages.warning(request, 'Oops, Something went wrong. Please try again later!')
+            form = CreateBookingForm() # An unbound form
+
     return render(request, 'booking/bookingcreate.html', context)
 
 def booking(request):
@@ -28,7 +39,7 @@ def booking(request):
                 context['booking_list'] = booking_list
 
         else:
-            form = ContactForm() # An unbound form
+            form = SearchBookingForm() # An unbound form
 
 
     return render(request, 'booking/booking.html', context)
